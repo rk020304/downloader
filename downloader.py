fb97603a7d1458d28f768f2cc913d24a6e7d6761
rk020304 <33202754+rk020304@users.noreply.github.com>
2017-10-31 16:29:13 +0900

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,3 +1,12 @@
+#動作環境
+#windows10 
+#python3.5.2 anacondaで仮想環境を作成して使用。
+
+#別添使用モジュール
+#Mecab 自然言語処理
+#natto-py Mecab と組み合わせて使用
+#google URL検索
+#OpenCV3 画像処理
 # coding:UTF-8
 
 import googledownload
