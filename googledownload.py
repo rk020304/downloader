eac4dba4ff831b4157f618a044b27941730a51e8
rk020304 <33202754+rk020304@users.noreply.github.com>
2017-10-31 16:28:56 +0900

    Update googledownload.py

--- googledownload.py
+++ googledownload.py
@@ -1,13 +1,3 @@
-#動作環境
-#windows10 
-#python3.5.2 anacondaで仮想環境を作成して使用。
-
-#別添使用モジュール
-#Mecab 自然言語処理
-#natto-py Mecab と組み合わせて使用
-#google URL検索
-#OpenCV3 画像処理
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import urllib
