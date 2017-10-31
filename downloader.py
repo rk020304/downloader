0cf728109355159e54d47a28f3c56b20440f81f7
rk020304 <33202754+rk020304@users.noreply.github.com>
2017-10-31 21:23:11 +0900

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -2,12 +2,14 @@
 #動作環境
 #windows10 
 #python3.5.2 anacondaで仮想環境を作成して使用。
-
 #別添使用モジュール
-#Mecab 自然言語処理
+#Mecab 自然言語処理 ←アプリケーションソフトウェア(mecab.exe)のインストールも必要。
 #natto-py Mecab と組み合わせて使用
 #google URL検索
 #OpenCV3 画像処理
+
+#モジュールやソフトウェアなどの環境設定の方法などはREADMEに記載
+
 # coding:UTF-8
 
 import googledownload
